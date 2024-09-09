a9b9fba3e61cab73096ff5d346684022a524bb41
mackeyaj <97459289+mackeyaj@users.noreply.github.com>
2024-09-09 07:48:50 -0400

    Update utils.groovy

--- libraries/debug/steps/utils.groovy
+++ libraries/debug/steps/utils.groovy
@@ -1,9 +1,9 @@
 package libraries.git.steps
 
 void call() {
-    def source_branch = git_distributions.fetch().get_source_branch()
+    //def source_branch = git_distributions.fetch().get_source_branch()
     node() {
     sh 'printenv'
-    println source_branch
+    //println source_branch
     }
 }
